9401e0a86d1ee8e71b3f07a6c89dad0eb25f11ae
Xavier Teo <xvrteo@gmail.com>
2023-04-20 15:48:58 -0700

    Edited models

--- models/User.js
+++ models/User.js
@@ -1,5 +1,5 @@
-const Thought = require("./Thought");
 const { Schema, model } = require('mongoose');
+
 const userSchema = new Schema(
   {
     username: {
@@ -12,7 +12,7 @@ const userSchema = new Schema(
       type: String,
       required: true,
       unique: true,
-      match: [/.+@.+\..+/, 'Must match an email address!'],
+      match: [/.+@.+\..+/, 'Must match an email address.'],
     },
     thoughts: [
       {
@@ -34,6 +34,7 @@ const userSchema = new Schema(
     id: false,
   }
 );
+
 userSchema.virtual('friendCount').get(function () {
   return this.friends.length;
 });
